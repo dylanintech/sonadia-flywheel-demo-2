9e4c1ea04754e238652d38a0348f561a9dd82e38
Dylan Molina <86082012+dylanintech@users.noreply.github.com>
2023-06-09 23:35:50 -0500

    added link to github

--- app/page.tsx
+++ app/page.tsx
@@ -176,7 +176,7 @@ export default function Home() {
     <main className="bg-[url('https://media.discordapp.net/attachments/993733319386730541/1116925999859384370/Screenshot_2023-06-09_at_10.04.42_PM.png?width=1620&height=1262')] bg-center bg-no-repeat bg-cover flex min-h-screen flex-col items-center justify-between p-2">
       <div className='flex flex-row items-center justify-between w-full'>
         <h1 className='text-white text-4xl font-mono'>sonadia flywheel demo!</h1>
-        <a href='https://github.com/dylanintech' target='none' className='p-2 rounded-full bg-black'>
+        <a href='https://github.com/dylanintech/sonadia-flywheel-demo-2' target='none' className='p-2 rounded-full bg-black'>
            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
         </a>
       </div>
